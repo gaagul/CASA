1bd45fd0f51e4102dc741f8a87effb6346e055bf
Gaagul Gigi <gaagulgigi@gmail.com>
2023-07-16 17:31:08 +0530

    Added pagination for listing Page

--- src/pages/ListingPage.jsx
+++ src/pages/ListingPage.jsx
@@ -4,11 +4,12 @@ import { useSearchParams } from "react-router-dom";
 import { pickBy, identity, isEmpty } from "ramda";
 import PropertyCard from "components/PropertyCard";
 import Filterbar from "components/Filterbar";
-import { Spin, Typography } from "antd";
+import { Spin, Typography, Pagination } from "antd";
 import { initializeFilters, filterProperties } from "../utils";
 import { useFetchProperties } from "../hooks/usePropertiesApi";
 
 const ListingPage = () => {
+  const [currentPage, setCurrentPage] = useState(1);
   const [filters, setFilters] = useState(initializeFilters());
   const [isFilterBarOpen, setIsFilterBarOpen] = useState(false);
   const [searchParams, setSearchParams] = useSearchParams();
@@ -57,9 +58,11 @@ const ListingPage = () => {
           />
           <div className="mt-4 flex flex-grow flex-wrap justify-start gap-6">
             {!isEmpty(filteredProperties) ? (
-              filteredProperties.map(property => (
-                <PropertyCard key={property.id} property={property} />
-              ))
+              filteredProperties
+                .slice((currentPage - 1) * 12, currentPage * 12)
+                .map(({ id, ...property }) => (
+                  <PropertyCard key={id} property={property} />
+                ))
             ) : (
               <div className="flex h-full w-full flex-col items-center justify-around">
                 <Typography.Text>No properties found</Typography.Text>
@@ -67,6 +70,13 @@ const ListingPage = () => {
             )}
           </div>
         </div>
+        <Pagination
+          className="mt-8 flex justify-center"
+          current={currentPage}
+          pageSize={12}
+          total={filteredProperties.length}
+          onChange={setCurrentPage}
+        />
       </Container>
     </div>
   );
