9d7d2872d4585e786dd4af05fc73850b7712f094
navaneeth-krishna <naveekurosaki@gmail.com>
2023-07-26 00:01:22 +0530

    Added api to fetch featured Properties

--- src/hooks/usePropertiesApi.js
+++ src/hooks/usePropertiesApi.js
@@ -1,10 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
-import { fetchAllProperties, fetchPropertyById } from "../apis/properties";
+import { fetchAllProperties, fetchPropertyById, fetchApprovedProperties, fetchFeaturedProperties } from "../apis/properties";
 
 const STALE_TIME = 100_000;
 const QUERY_KEYS = {
   PROPERTIES_LIST: "propertiesList",
   PROPERTY_BY_ID: "propertyById",
+  APPROVED_PROPERTIES: "approvedProperties",
+  FEATURED_PROPERTIES: "featuredProperties"
 };
 
 const useFetchProperties = () =>
@@ -17,4 +19,18 @@ const useFetchProperties = () =>
 const useFetchPropertyById = id =>
   useQuery([QUERY_KEYS.PROPERTY_BY_ID], () => fetchPropertyById(id));
 
-export { useFetchProperties, useFetchPropertyById };
+const useFetchApprovedProperties = () => 
+  useQuery([QUERY_KEYS.APPROVED_PROPERTIES], fetchApprovedProperties, {
+  select: data =>
+      data?.docs.map(doc => ({ ...doc.data(), id: doc.id, key: doc.id })),
+    staleTime: STALE_TIME,
+  })
+
+const useFetchFeaturedAssets = () => 
+  useQuery([QUERY_KEYS.FEATURED_PROPERTIES], fetchFeaturedProperties, {
+  select: data =>
+      data?.docs.map(doc => ({ ...doc.data(), id: doc.id, key: doc.id })),
+    staleTime: STALE_TIME,
+  })
+
+export { useFetchProperties, useFetchPropertyById, useFetchApprovedProperties, useFetchFeaturedAssets };
