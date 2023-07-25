8a0900e1f22c633d9dbe44851f3d98a1c65b3c44
navaneeth-krishna <naveekurosaki@gmail.com>
2023-07-26 00:06:12 +0530

    Modified to Login using Google Auth Provider Only

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -32,20 +32,6 @@ const defaultTheme = createTheme();
 
 const Login = () => {
   const location = useLocation();
-
-  const handleSubmit = event => {
-    event.preventDefault();
-    // const data = new FormData(event.currentTarget);
-    setToLocalStorage("isLoggedIn", true);
-    const { state = {} } = location;
-    let redirectPath = state?.from || "/";
-
-    if (state?.queryParams) {
-      redirectPath = `${redirectPath}?${state.queryParams}`;
-    }
-    window.location.href = redirectPath;
-  };
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <Grid container component="main" sx={{ height: "100vh" }}>
@@ -77,7 +63,7 @@ const Login = () => {
               alignItems: "center",
             }}
           >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <Avatar sx={{ m: 1, mt: 15, bgcolor: "secondary.main" }}>
               <LockOutlined />
             </Avatar>
             <Typography component="h1" variant="h5">
@@ -88,52 +74,7 @@ const Login = () => {
               className="!max-w-[60%]"
               component="form"
               sx={{ mt: 1 }}
-              onSubmit={handleSubmit}
             >
-              <TextField
-                autoFocus
-                fullWidth
-                required
-                autoComplete="email"
-                id="email"
-                label="Email Address"
-                margin="normal"
-                name="email"
-              />
-              <TextField
-                fullWidth
-                required
-                autoComplete="current-password"
-                id="password"
-                label="Password"
-                margin="normal"
-                name="password"
-                type="password"
-              />
-              <FormControlLabel
-                control={<Checkbox color="primary" value="remember" />}
-                label="Remember me"
-              />
-              <Button
-                fullWidth
-                sx={{ mt: 3, mb: 2 }}
-                type="submit"
-                variant="contained"
-              >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    Don't have an account? Sign Up
-                  </Link>
-                </Grid>
-              </Grid>
               <GoogleAuth redirectPath="/listing" />
               <Copyright sx={{ mt: 5 }} />
             </Box>
